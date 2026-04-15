ffc015c446f2fd3c0a188538b21e0fd06c4457e5
Raze <skwasefmostafa3@gmail.com>
2026-04-16 01:33:01 +0530

    Initial commit - Zefi project

---
+++ components/header.jsx
@@ -0,0 +1,66 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { LayoutDashboard, PenBox } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="Zefi Logo"
+            width={100}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center space-x-4">
+          <Show when="signed-in">
+            <Link href="/dashboard">
+              <Button
+                variant="outline"
+                className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+              >
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+
+            <Link href="/transaction/create">
+              <Button>
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </Link>
+          </Show>
+          <Show when="signed-out">
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+
+            {/* <SignUpButton>
+            <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton> */}
+          </Show>
+          <Show when="signed-in">
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            />
+          </Show>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/hero.jsx
@@ -0,0 +1,68 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+  const imgRef = useRef();
+  useEffect(() => {
+    const imageElement = imgRef.current;
+
+    const handleScroll = () => {
+      if (!imageElement) return;
+
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div className="pb-20 px-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[150px] pb-6 gradient-title">
+          Take Control of Your Finances with AI
+        </h1>
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+          An AI-powered financial management platform that helps you track your
+          expenses, analyze your spending patterns, and gain insights into your
+          spending habits.
+        </p>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="">
+            <Button size="lg" variant="outline" className="px-8">
+              Watch Demo
+            </Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper">
+          <div ref={imgRef} className="hero-image">
+            <Image
+              src="/Landing_Page.jpg"
+              alt="Dashboard Preview"
+              width={1280}
+              height={720}
+              priority
+              className="rounded-lg shadow-2xl border mx-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
